19034b613361a11ed7a17678480a1d24c4ab8afa
kenDevelop6655 <kenblack7715@gmail.com>
2023-08-07 22:14:47 +0900

    first commit

---
+++ src/main/java/com/example/ems_gradle/EmsGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ems_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsGradleApplication.class, args);
+	}
+
+}
