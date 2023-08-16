707cb3ed7140842391e594ec7964980a064fe306
kenDevelop6655 <kenblack7715@gmail.com>
2023-08-16 22:42:01 +0900

    備品の追加の実装完了

--- src/main/java/com/example/ems_gradle/app/service/EquipService.java
+++ src/main/java/com/example/ems_gradle/app/service/EquipService.java
@@ -1,9 +1,16 @@
 package com.example.ems_gradle.app.service;
+import com.example.ems_gradle.domain.mbgModel.ems.EquipType;
+import com.example.ems_gradle.domain.mbgModel.ems.EquipTypeExample;
+import com.example.ems_gradle.infra.mbgMapper.ems.EquipTypeMapper;
 import com.example.ems_gradle.infra.mbgMapper.ems.ManageEquipMapper;
-import com.example.ems_gradle.infra.mbgModel.ems.ManageEquip;
+import com.example.ems_gradle.domain.mbgModel.ems.ManageEquip;
+import com.example.ems_gradle.infra.myMapper.ems.MyManageEquipMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -11,8 +18,26 @@ public class EquipService {
 
     @Autowired
     ManageEquipMapper manageEquipMapper;
+    MyManageEquipMapper myManageEquipMapper;
+    @Autowired
+    EquipTypeMapper equipTypeMapper;
 
+
+    //管理備品の一覧を返す
+    public List<ManageEquip> findAll(){
+        List<ManageEquip> equipList = myManageEquipMapper.findAll();
+        return equipList;
+    }
+
+    //管理備品を追加する
     public void insertEquip(ManageEquip equip){
-        manageEquipMapper.insert(equip);
+        manageEquipMapper.insertSelective(equip);
+    }
+
+    //備品タイプの一覧を返す
+    public List<EquipType> findAllType(){
+        EquipTypeExample equipTypeExample = new EquipTypeExample();
+        equipTypeExample.setOrderByClause("equip_type_id");
+        return  equipTypeMapper.selectByExample(equipTypeExample);
     }
 }
--- src/main/java/com/example/ems_gradle/presentation/controller/EquipController.java
+++ src/main/java/com/example/ems_gradle/presentation/controller/EquipController.java
@@ -1,11 +1,16 @@
 package com.example.ems_gradle.presentation.controller;
 
 import com.example.ems_gradle.app.service.EquipService;
-import com.example.ems_gradle.infra.mbgModel.ems.ManageEquip;
+import com.example.ems_gradle.domain.mbgModel.ems.EquipType;
+import com.example.ems_gradle.domain.mbgModel.ems.ManageEquip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("equip")
@@ -14,15 +19,34 @@ public class EquipController {
     @Autowired
     EquipService equipService;
 
-    ManageEquip manageSample = new ManageEquip();
 
 
+    //一覧ページの表示
+    @RequestMapping("")
+    String home(Model model){
+        model.addAttribute("sample","sample");
+        return "home";
+    }
 
-    @RequestMapping("insert")
-    String insertEquip(Model model){
-        manageSample.setEquipId(1);
-        manageSample.setEquipName("PC");
-        equipService.insertEquip(manageSample);
+    //管理備品の追加画面の表示
+    @RequestMapping("addForm")
+    String addForm(Model model){
+        EquipType equipType = new EquipType();
+        ManageEquip manageSample = new ManageEquip();
+        List<EquipType> equipTypeList = equipService.findAllType();
+        model.addAttribute("equipForm",manageSample);
+        model.addAttribute("equipType",equipType);
+        model.addAttribute("equipTypeList",equipTypeList);
+        return "add_form";
+    }
+
+    //管理備品の追加
+    @RequestMapping("add")
+    String insertEquip(
+            @Validated ManageEquip manageEquip,
+            BindingResult bindingResult,
+            Model model){
+        equipService.insertEquip(manageEquip);
         return "home";
     }
 
